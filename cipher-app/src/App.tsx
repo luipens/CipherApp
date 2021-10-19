83639000332e35e1b56b1d27b6eafcaef3adfcab
Luis Pensado <luis.m.pensado@gmail.com>
2021-10-18 21:21:59 -0500

    Added basic UI elements

--- cipher-app/src/App.tsx
+++ cipher-app/src/App.tsx
@@ -1,44 +1,100 @@
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import Page from './pages/Page';
+import {
+  IonApp,
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { calculatorOutline, key, refreshOutline } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+import { useRef } from "react";
 
 const App: React.FC = () => {
+  const plaintextInputRef = useRef<HTMLIonInputElement>(null);
+  const keyInputRef = useRef<HTMLIonInputElement>(null);
+
+  const calculateCipher = () => {
+    const enteredPlaintext = plaintextInputRef.current!.value;
+    const enteredKey = keyInputRef.current!.value;
+  };
+
+  const resetInputs = () => {
+    plaintextInputRef.current!.value = '';
+    keyInputRef.current!.value = '';
+  };
+
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/page/Inbox" />
-            </Route>
-            <Route path="/page/:name" exact={true}>
-              <Page />
-            </Route>
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Cipher Generator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position = "fixed">Your Plaintext</IonLabel>
+                <IonInput ref={plaintextInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonItem>
+                <IonLabel position = "fixed">Your Key</IonLabel>
+                <IonInput ref={keyInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className="ion-text-left">
+              <IonButton onClick={calculateCipher}>
+                <IonIcon slot="start" icon={calculatorOutline} />
+                Calculate
+              </IonButton>
+            </IonCol>
+            <IonCol className="ion-text-right">
+              <IonButton onClick={resetInputs}>
+                <IonIcon slot="start" icon={refreshOutline} />
+                Reset
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonApp>
   );
 };
